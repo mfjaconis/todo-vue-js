5400cff403d15fa02a4428a795375b335101b1e3
mfjaconis <matheusjaconis@gmail.com>
2025-02-11 16:22:05 -0300

    html do header

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,15 @@
 
 </script>
 
-
+<template>
+    <div class="container">
+        <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+            <h1>Minhas tarefas</h1>
+            <p>
+                Você possui 7 tarefas pendentes
+            </p>
+        </header>
+    </div>
 </template>
 
 <style scoped></style>
