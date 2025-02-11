cff2a49b443c9dbf8c817b7251c14c073c7ce8d6
mfjaconis <matheusjaconis@gmail.com>
2025-02-11 16:28:07 -0300

    criação do formulário

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,23 @@
                 Você possui 7 tarefas pendentes
             </p>
         </header>
+        <form>
+            <div class="row">
+                <div class="col">
+                    <input type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+                </div>
+                <div class="col-md-1">
+                    <button type="submit" class="btn btn-primary">Cadastrar</button>
+                </div>
+                <div class="col-md-2">
+                    <select class="form-control">
+                        <option value="todas">Todas tarefas</option>
+                        <option value="pendentes">pendentes</option>
+                        <option value="finalizadas">Finalizadas</option>
+                    </select>
+                </div>
+            </div>
+        </form>
     </div>
 </template>
 
